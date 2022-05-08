38e4cf38a899ca596ee6e36e6dd5fa49f3ad8d42
orshk <sdi@proekt08.ru>
2022-05-08 16:17:26 +0300

    Все дашборды (с заглушками)

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,10 +3,36 @@ const express = require('express');
 const YAML = require('yamljs');
 const config = require('./config');
 
-const authDocument = YAML.load('./openapi3-auth.yaml');
+const authDocument = YAML.load('./openapi3/auth.yaml');
+const taskDocument = YAML.load('./openapi3/auth.yaml');
+const billingDocument = YAML.load('./openapi3/auth.yaml');
+const analyticsDocument = YAML.load('./openapi3/auth.yaml');
 
-const app = express()
-app.use('/auth', swaggerUI.serve, swaggerUI.setup(authDocument));
+const app = express();
+
+app.use('/auth', function(req, res, next){
+  authDocument.servers[0].url = `http://localhost:${config.auth_port}`;
+  req.swaggerDoc = authDocument;
+  next();
+}, swaggerUI.serve, swaggerUI.setup());
+
+app.use('/task', function(req, res, next){
+  taskDocument.servers[0].url = `http://localhost:${config.tasks_port}`;
+  req.swaggerDoc = taskDocument;
+  next();
+}, swaggerUI.serve, swaggerUI.setup());
+
+app.use('/billing', function(req, res, next){
+  billingDocument.servers[0].url = `http://localhost:${config.billing_port}`;
+  req.swaggerDoc = billingDocument;
+  next();
+}, swaggerUI.serve, swaggerUI.setup());
+
+app.use('/analytics', function(req, res, next){
+  analyticsDocument.servers[0].url = `http://localhost:${config.analytic_port}`;
+  req.swaggerDoc = analyticsDocument;
+  next();
+}, swaggerUI.serve, swaggerUI.setup());
 
 const server = app.listen(config.port, () => {
   const host = server.address().address;
