23c2459a89f9bdc20b189ba07390c3f015f7fd0a
Denis Saygin <oreshka.net@gmail.com>
2022-05-12 10:14:39 +0300

    Users service

--- tasktracker/internal/services/users.go
+++ tasktracker/internal/services/users.go
@@ -1,11 +1,29 @@
 package services
 
-import "github.com/oreshkanet/aTES/tasktracker/internal/transport"
+import (
+	"github.com/oreshkanet/aTES/tasktracker/internal/repository"
+	"github.com/oreshkanet/aTES/tasktracker/internal/transport"
+)
 
 type UsersService struct {
+	repos *repository.UserRepository
 }
 
 func (s *UsersService) HandleUserMessage(message *transport.UserMessage) error {
+	//ctx := context.Background()
+	switch message.Operation {
+	case "C":
+		// TODO: Добавление пользователя в систему
+		break
+	case "U":
+		// TODO: Обновление пользователя в системе
+		break
+	case "D":
+		// TODO: Удаление пользователя из системы
+		break
+	default:
+		// TODO: Неизвестная операция
+	}
 	// TODO: Обработка сообщений из кафки
 	return nil
 }
