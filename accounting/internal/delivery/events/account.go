8f58227f31071578c397f8c63bfdda39b3b9bf1d
Denis Saygin <oreshka.net@gmail.com>
2022-05-15 10:10:14 +0300

    AccountingService

---
+++ accounting/internal/delivery/events/account.go
@@ -0,0 +1,29 @@
+package events
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/oreshkanet/aTES/accounting/internal/domain"
+)
+
+func (p *Producer) TransactStream(ctx context.Context, message *domain.TransactStreamMessage) error {
+	// TODO: Поработать с контекстом и сделать тайм-аут или вообще сделать асинхронную отправку через горутину
+	msg, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	p.transactStreamCh <- msg
+
+	return err
+}
+
+func (p *Producer) TransactPayment(ctx context.Context, message *domain.TransactPaymentMessage) error {
+	// TODO: Поработать с контекстом и сделать тайм-аут или вообще сделать асинхронную отправку через горутину
+	msg, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	p.transactPaymentCh <- msg
+
+	return err
+}
