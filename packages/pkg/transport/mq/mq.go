1a111f9740170f2bdd1e9cc4fe1862c12ac8cece
Denis Saygin <oreshka.net@gmail.com>
2022-05-21 20:55:25 +0300

    Рефакторинг mq-транспорта

--- packages/pkg/transport/mq/mq.go
+++ packages/pkg/transport/mq/mq.go
@@ -1,17 +1,21 @@
 package mq
 
-import "context"
+import (
+	"context"
+)
 
 type MessageBroker interface {
-	Produce(ctx context.Context, topic string, messageChannel <-chan []byte) error
-	Consume(ctx context.Context, topic string, messageChannel chan<- []byte) error
+	Produce(ctx context.Context, topic *Topic) (Producer, error)
+	Consume(ctx context.Context, topic *Topic) (Consumer, error)
 	Close()
 }
 
 type Producer interface {
-	Publish(message []byte, messageChannel <-chan []byte)
+	Run(ctx context.Context)
+	Publish(message []byte) error
 }
 
 type Consumer interface {
-	Consume(ctx context.Context, messageChannel chan<- []byte)
+	Run(ctx context.Context)
+	Read() <-chan []byte
 }
